7d9d612fa5893e64b3e4ca4c2334ffee059cb82d
DavidMiles-Skov <milesskovd@gmail.com>
2024-08-27 08:19:13 +0200

    added initial maps and pricing data

--- code/main.py
+++ code/main.py
@@ -1,3 +1,4 @@
+print("Hello")
 import folium
 import geopandas as gpd
 import geodatasets 
@@ -28,14 +29,16 @@ def load_geojson(path='data/europe.geojson'):
     
 
 def load_price(index, path='data/pricing/'):
-    datapath = f"{path}/price_{index}.csv"
+    datapath = f"{path}/electricity_prices_{index}.csv"
     price_data = pd.read_csv(datapath)
     return price_data
     
 
 
 
-def make_map():
+def make_and_save_map(index):
+
+    price_data = load_price(index)
 
     map_size = 20 # Determine size of map around cph (Doesn't appear to change anything in notebook.)
     min_lon, max_lon = cph[0]-map_size, cph[0]+map_size
@@ -49,5 +52,30 @@ def make_map():
                             zoom_control=False)
     colormap = linear.YlGn_09.scale(
     -10, 210)
+    eu_geojson = load_geojson()
 
-    return
+    folium.Choropleth(
+    geo_data=eu_geojson,
+    data = price_data,
+    columns=['Country', 'Price'],
+    key_on= 'feature.properties.NAME',
+    fill_color="YlGn",
+    fill_opacity=0.7,
+    line_opacity=0.2,
+    highlight=True,
+    legend_name="Price (€/MWh)"
+    ).add_to(nordpool_map)
+
+    folium.LayerControl().add_to(nordpool_map)
+
+    # saving
+    nordpool_map.save(f'maps/interactive/nordpool_{index}.html')
+    return None
+
+def main():
+    for i in range(1, 11):
+        make_and_save_map(i)
+
+
+if __name__=="__main__":
+    main()
